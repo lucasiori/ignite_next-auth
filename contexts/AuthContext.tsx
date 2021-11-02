66913a8ee16f7deb247dd689f4173b08ec6958d8
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-02 18:26:38 -0300

    feat: create automatic logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import Router from 'next/router';
-import { setCookie, parseCookies } from 'nookies';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import { api } from '../services/api';
 
 type User = {
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, 'next-auth.token');
+  destroyCookie(undefined, 'next-auth.refreshToken');
+
+  Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
@@ -38,7 +45,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
         const { email, permissions, roles } = response.data;
 
         setUser({ email, permissions, roles });
-      })
+      }).catch(() => {
+        signOut();
+      });
     }
   }, []);
 
