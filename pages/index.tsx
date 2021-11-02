bcc39973c0027f6fd927c155d325b34ebc4d0cf3
Lucas Fernando Iori <lucas.iori@fcamara.com.br>
2021-11-02 17:18:33 -0300

    feat: create auth context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,39 @@
+import { FormEvent, useState } from 'react';
+import { useAuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useAuthContext();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password
+    };
+
+    await signIn(data);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={event=> setPassword(event.target.value)}
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
