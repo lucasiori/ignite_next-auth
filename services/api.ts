a48275b3d2df4aeb72b9cbdb77f781f9f8c7b58c
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-02 18:08:36 -0300

    feat: configure refresh token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,42 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['next-auth.token']}`
+  }
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies();
+      const { 'next-auth.refreshToken': refreshToken } = cookies;
+
+      api.post('refresh', {
+        refreshToken
+      }).then(response => {
+        const { token, refreshToken: newRefreshToken } = response.data;
+
+        setCookie(undefined, 'next-auth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        });
+  
+        setCookie(undefined, 'next-auth.refreshToken', newRefreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        });
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      });
+    } else {
+
+    }
   }
 });
