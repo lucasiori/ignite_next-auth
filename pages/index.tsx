f67b78eb7c35271ec949664d4654723aa5560dc4
Lucas Fernando Iori <lucasferiori@gmail.com>
2021-11-06 15:03:46 -0300

    feat: customize layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,21 +21,25 @@ export default function Home() {
   }
 
   return (
-    <form className={styles.container} onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-      />
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <h1>Next Auth</h1>
+        
+        <input
+          type="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
 
-      <input
-        type="password"
-        value={password}
-        onChange={event=> setPassword(event.target.value)}
-      />
+        <input
+          type="password"
+          value={password}
+          onChange={event=> setPassword(event.target.value)}
+        />
 
-      <button type="submit">Entrar</button>
-    </form>
+        <button type="submit">Entrar</button>
+      </form>
+    </main>
   )
 }
 
